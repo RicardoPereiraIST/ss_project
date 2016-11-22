f4827f9c25373700a1fff1ffc9b2dc2b4556867e
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2016-11-22 23:39:33 +0000

    Add some implementation

--- analyzer.py
+++ analyzer.py
@@ -1,19 +1,70 @@
 import sys
+import re
 
 def importFile(filename):
+	dictionary_list = []
 	try:
-		slice_lines = [line.rstrip('\n') for line in open(filename)]
+		with open(filename) as f:
+			slice_lines = f.read().split(';\n')
+			slice_lines.pop()
+			
+			for line in slice_lines:
+				instruction = line.split('=', 1)
+				variable_list = [instruction[0],instruction[1].replace(';\n', '') , False]
+				dictionary_list.append(variable_list)
+
+		f.close()
+
 	except:
-		print "Error opening file"
+		print ("Error opening file")
 		sys.exit(1)
 
-	return slice_lines
+	return dictionary_list
+
+def importPatterns(filename):
+	try:
+		with open(filename) as f:
+			patterns = f.readlines()
+
+			result_patterns = []
+
+			for line in patterns:
+				if line != '\n':
+					line = line.replace('\n', '')
+					result_patterns.append(line)
+		f.close()
+	except:
+		print ("Error opening file")
+		sys.exit(1)
+
+	structured_patterns = []
+	for i in range(0, len(result_patterns), 4):
+		temp = []
+		for j in range(4):
+			temp.append(result_patterns[j])
+		structured_patterns.append(temp)
+
+	return structured_patterns
+
+def substituteVariables(variable_list):
+	for i in range(1, len(variable_list)):
+		variable_list[i][1] = variable_list[i][1].replace(variable_list[i-1][0], variable_list[i-1][1])
+
+	return variable_list[-1][1]
+
 
 def checkArgs():
 	if len(sys.argv) != 2:
-		print "Usage: python analyzer.py <slice>"
+		print ("Usage: python analyzer.py <slice>")
 		sys.exit(1)
 
-checkArgs()
-lines = importFile(sys.argv[1])
 
+if __name__ == "__main__":
+	checkArgs()
+	variable_list = importFile(sys.argv[1])
+
+	patterns = importPatterns("proj-patterns/patterns")
+
+	parsed_instruction = substituteVariables(variable_list)
+
+	print(parsed_instruction)
