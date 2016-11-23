e1c4e5e2d08865d8098219aedbc75a060d629d6c
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2016-11-23 23:36:17 +0000

    Add taint analysis algorithm with graph structure, add secure slice to test

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import networkx as nx
 
 def importFile(filename):
 	result_list = []
@@ -26,12 +27,11 @@ def importFile(filename):
 			if(temp_line != ""):
 				parsed_lines.append(temp_line)
 
-
 			for line in parsed_lines:
 				if line[0] == '$':
 					instruction = line.split('=', 1)
 					if instruction[1][-1] == ';':
-						variable_list = [instruction[0],instruction[1].replace(';', '') , False]
+						variable_list = [instruction[0],instruction[1].replace(';', ''), False]
 					else:
 						variable_list = [instruction[0],instruction[1], False]
 					result_list.append(variable_list)
@@ -44,7 +44,6 @@ def importFile(filename):
 		print ("Error opening file")
 		sys.exit(1)
 
-	print(result_list)
 	return result_list
 
 def importPatterns(filename):
@@ -58,6 +57,7 @@ def importPatterns(filename):
 				if line != '\n':
 					line = line.replace('\n', '')
 					result_patterns.append(line)
+
 		f.close()
 	except:
 		print ("Error opening file")
@@ -66,57 +66,115 @@ def importPatterns(filename):
 	structured_patterns = []
 	for i in range(0, len(result_patterns), 4):
 		temp = []
-		for j in range(4):
-			temp.append(result_patterns[j])
+		temp.append(result_patterns[i])
+		temp.append(result_patterns[i+1])
+		temp.append(result_patterns[i+2])
+		temp.append(result_patterns[i+3])
 		structured_patterns.append(temp)
 
 	return structured_patterns
-
-def substituteVariables(variable_list):
-	
-
 
 def checkArgs():
 	if len(sys.argv) != 2:
 		print ("Usage: python analyzer.py <slice>")
 		sys.exit(1)
 
+def findPattern(variable_list, patterns):
+	found_pattern = []
+
+	i = len(variable_list) - 1
+	for pattern in patterns:
+		sensitive_sinks = pattern[3].split(',')
+		for sensitive_sink in sensitive_sinks:
+			if sensitive_sink in variable_list[i][1]:
+				found_pattern = pattern
+				break
+		if(found_pattern != []):
+			break
+
+	return found_pattern, sensitive_sink
+
+
+def createGraph(slice_list):
+	graph = nx.DiGraph()
+	program_line_number = 0
+	for instruction in slice_list:
+		graph.add_node(
+			program_line_number, tainted = False, variable = instruction[0], body = instruction[1])
+		program_line_number+=1
+
+	var_list = []
+	if len(graph.nodes()) != 1:
+		for node in graph:
+			potential_vars = re.findall(r'\$\w+', graph.nodes(data=True)[node][1]['body'])
+			for var in potential_vars:
+				if var in var_list:
+					graph.add_edge(var_list[var_list.index(var)+1],node)
+			if graph.nodes(data=True)[node][1]['variable'] != '':
+				var_list.append(graph.nodes(data=True)[node][1]['variable'].replace(' ', ''))
+				var_list.append(node)
+
+	return graph
+
+def correctGraph(graph):
+	graph.add_node(-1)
+	for node in graph:
+		if graph.predecessors(node) == [] and node != -1:
+			graph.add_edge(-1, node)
+
+
+def traverseGraph(graph,pattern,sensitive_sink):
+	result = []
+
+	for node in sorted(graph):
+		successors = graph.successors(node)
+
+		if node != -1:
+			entry_points = pattern[1].split(',')
+			sanitization_functions = pattern[2].split(',')
+
+			if successors == []:
+				last_node = node
+				sink_args = graph.nodes(data=True)[node][1]['body'].split(sensitive_sink)[1:][0].replace(' ', '')
+				for entry in entry_points:
+					if entry in sink_args:
+						graph.node[node]['tainted'] = True
+				for function in sanitization_functions:
+					if function in sink_args:
+						graph.node[node]['tainted'] = False
+			else:
+				body = graph.nodes(data=True)[node][1]['body']
+				for entry in entry_points:
+					if entry in body:
+						graph.node[node]['tainted'] = True
+				for function in sanitization_functions:
+					if function in body:
+						graph.node[node]['tainted'] = False
+
+			for successor in successors:
+				if graph.nodes(data=True)[node][1]['tainted'] == True:
+					graph.node[successor]['tainted'] = True
+
+
+	result.append(graph.nodes(data=True)[last_node][1]['tainted'])
+	if result[0] == True:
+		result.append(pattern[0])
+	else:
+		result.append("Seguro")
+	return result
+
 
 if __name__ == "__main__":
 	checkArgs()
-	variable_list = importFile(sys.argv[1])
 
+	slice_list = importFile(sys.argv[1])
 	patterns = importPatterns("proj-patterns/patterns")
 
-	#parsed_instruction = substituteVariables(variable_list)
+	found_pattern, sensitive_sink = findPattern(slice_list, patterns)
 
-	content = []
-	flag = False
+	g = createGraph(slice_list)
+	correctGraph(g)
 
-	#print(parsed_instruction)
+	result = traverseGraph(g, found_pattern, sensitive_sink)
 
-	"""for pattern in patterns:
-		sensitive_sinks = pattern[3].split(',')
-		for sink in sensitive_sinks:
-			if re.compile(sink + '\((.*?)\)').findall(parsed_instruction) != []:
-				content = re.compile(sink + '\((.*?)\)').findall(parsed_instruction)
-				content = content[0].split(',')
-
-		inner_inner_content = []
-		sanitization_functions = pattern[2].split(',')
-		for function in sanitization_functions:
-			for inner_content in content:
-				if re.compile(function + '\((.*?)\)').findall(inner_content) != []:
-					inner_inner_content = re.compile(sink + '\((.*?)\)').findall(inner_content)
-					flag = True
-					break
-
-		if(flag != True):
-			entry_points = pattern[1].split(',')
-			for entry_point in entry_points:
-				for inner_content in content:
-					if re.compile(entry_point.replace('$','\$') + '\[(.*?)\]').findall(inner_content) != []:
-						print("Inseguro")
-						sys.exit(1)
-
-	print("Seguro") """
+	print (result)
